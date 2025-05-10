3b2fc76e22e7260c0528947256937c12f247748a
SoonHyung_LEE <tnsgud9@naver.com>
2025-05-10 17:38:33 +0900

    Fix Dummy Route Config

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,55 @@
-import { createBrowserRouter, RouterProvider } from "react-router";
-import Home from "./pages/Home";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router";
 
 const App = () => {
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Home />,
+      element: (
+        <div>
+          Root Layout
+          <Outlet />
+        </div>
+      ),
+      children: [
+        {
+          path: "auth",
+          element: (
+            <div>
+              Auth Layout
+              <Outlet />
+            </div>
+          ),
+          children: [
+            {
+              path: "login",
+              element: <div>Login Page</div>,
+            },
+            {
+              path: "signup",
+              element: <div>Signup Page</div>,
+            },
+          ],
+        },
+        {
+          path: "chat",
+          element: (
+            <div>
+              Chat Layout
+              <Outlet />
+            </div>
+          ),
+          children: [
+            {
+              path: "rooms",
+              element: <div>Chat Rooms Page</div>,
+            },
+            {
+              path: ":roomId",
+              element: <div>Chat Room Page</div>,
+            },
+          ],
+        },
+      ],
     },
   ]);
   return <RouterProvider router={router} />;
